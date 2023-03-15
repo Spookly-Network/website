09dddcd8e6b2138a7070cd1b3fce4f6c6e842ffe
Niklas Ehlen <niklas.ehlen@pixlinemedia.de>
2023-03-15 23:52:18 +0100

    Change css for materials

--- resources/views/components/material_list.blade.php
+++ resources/views/components/material_list.blade.php
@@ -1,7 +1,7 @@
 {!! Arr::get($blok, '_editable', '') !!}
 <div class="row">
     @foreach ($blok['body'] as $blok)
-        <div class="col-xxl-2 col-md-4 col-6">
+        <div class="col-xxl-2 col-md-4 col-6 mb-2">
             <x-dynamic-component :component="$blok['component']" :blok="$blok"/>
         </div>
     @endforeach
